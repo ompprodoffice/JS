33cf0ae80d0b2dc02779356d0834a8533c93fe98
ompprodoffice <ompprodoffice@gmail.com>
2025-11-20 11:12:58 +0800

    Add files via upload

---
+++ print-PPE-v1.1.js
@@ -0,0 +1,42 @@
+(()=>{
+    'use strict';
+    async function getTemplateKey() {
+      const body = {
+        app: 868,
+        id: 10,
+      };
+      try {
+        const resp = await kintone.api(
+          kintone.api.url("/k/v1/record", true),
+          "GET",
+          body
+        );
+        // console.log(resp);
+        return resp.record.attachment.value[0].fileKey;
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    async function downloadTemplateFile(fileKey) {
+      
+    const url =
+      kintone.api.url("/k/v1/file.json", true) +
+      "?fileKey=" +
+      encodeURIComponent(fileKey);
+    const resp = await fetch(url, {
+      method: "GET",
+      headers: { "X-Requested-With": "XMLHttpRequest" },
+      credentials: "include",
+    });
+    if (!resp.ok)
+      throw new Error(`Failed to download file. HTTP ${resp.status}`);
+    return await resp.blob();
+    }
+    kintone.events.on("app.record.detail.show", async (event)=>{
+        const record = event.record;
+        const fileKey = await getTemplateKey();
+        console.log(fileKey);
+        await downloadTemplateFile(fileKey);
+        return event;
+    });
+})()
